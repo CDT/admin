490d77c552891f91172ede5151bd44b45f70fc89
dtchen <779888925@qq.com>
2018-09-04 17:26:01 +0800

    '.'

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,15 @@
+
+
 <template>
   <div>
+    <notifications></notifications>
     <router-view></router-view>
   </div>
 </template>
+
+<script>
+export default {}
+</script>
 
 <style>
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,31 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import { directive as vClickOutside } from "vue-clickaway"
+import Notify from "vue-notifyjs"
 
 // 导入样式
 import "bootstrap/dist/css/bootstrap.css"
 import "@fortawesome/fontawesome-free/css/all.css"
 import "@/assets/css/admin.scss"
 
+// 注册全局组件
+// Vue.component("fg-input", FormGroupInput)
+// Vue.component("drop-down", DropDown)
+// Vue.component("card", Card)
+// Vue.component("p-button", Button)
+
+// 注册全局Directive
+Vue.directive('click-outside', vClickOutside)
+
+// 注册SideBar和Notify
+// Vue.use(SideBar)
+Vue.use(Notify)
+
+
 new Vue({
   router,
   store,
   render: h => h(App),
-}).$mount('#app');
+}).$mount('#app')
